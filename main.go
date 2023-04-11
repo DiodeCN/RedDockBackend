e646e104b86c767835319ec8c4317e168ac109dd
DiodeCN <1069523953@qq.com>
2023-04-12 05:46:33 +0800

    hhh

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 	tweetsCollection := twitterDatabase.Collection("Tweets")
 	usersCollection := twitterDatabase.Collection("Users")
 
-	register.JustSendASMS()
+	register.RegisterSMS([]string{"+8615807989053"}, []string{"1758459"})
 
 	r.GET("/api/tweets", func(c *gin.Context) {
 		tweets, err := tweet.GetAllTweets(ctx, tweetsCollection)
