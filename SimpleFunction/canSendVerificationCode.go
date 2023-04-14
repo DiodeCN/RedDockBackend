a732588995c1e31e7a084e17bef495ff3888afeb
DiodeCN <1069523953@qq.com>
2023-04-14 22:11:35 +0800

    提交咯

---
+++ SimpleFunction/canSendVerificationCode.go
@@ -0,0 +1,17 @@
+package canSendVerificationCode
+
+import "time"
+
+var ipPhoneTimers = make(map[string]time.Time)
+
+func canSendVerificationCode(ip string, phoneNumber string) bool {
+	key := ip + ":" + phoneNumber
+	lastSendTime, exists := ipPhoneTimers[key]
+
+	if exists && time.Since(lastSendTime) < 60*time.Second {
+		return false
+	}
+
+	ipPhoneTimers[key] = time.Now()
+	return true
+}
