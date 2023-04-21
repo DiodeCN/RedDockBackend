c6562828b9bb2e25880be336c096593f6bac4273
DiodeCN <1069523953@qq.com>
2023-04-21 16:24:10 +0800

    哈希！

--- RefactoredModule/Hash/hash.go
+++ RefactoredModule/Hash/hash.go
@@ -17,35 +17,20 @@ func init() {
 // 加密函数
 func HashString(str string) (string, error) {
 	key := os.Getenv("HASH_KEY")
-
-	if len(key) == 0 {
-		return "", fmt.Errorf("Hash key not found in .env file")
-	}
-
 	hashed, err := bcrypt.GenerateFromPassword([]byte(key+str), bcrypt.DefaultCost)
-
 	if err != nil {
 		return "", err
 	}
-
 	return string(hashed), nil
 }
 
 // 解密函数
 func CheckHash(hashed, str string) bool {
 	key := os.Getenv("HASH_KEY")
-
-	if len(key) == 0 {
-		fmt.Println("Hash key not found in .env file")
-		return false
-	}
-
 	err := bcrypt.CompareHashAndPassword([]byte(hashed), []byte(key+str))
-
 	if err != nil {
 		fmt.Println(err)
 		return false
 	}
-
 	return true
 }
