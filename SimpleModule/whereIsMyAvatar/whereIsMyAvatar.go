1d8ab32b7419fbe22c234f75c9125043dee506d1
DiodeCN <1069523953@qq.com>
2023-04-25 05:29:57 +0800

    无所谓

--- SimpleModule/whereIsMyAvatar/whereIsMyAvatar.go
+++ SimpleModule/whereIsMyAvatar/whereIsMyAvatar.go
@@ -1,6 +1,8 @@
 package whereismyavatar
 
 import (
+	"net/http"
+	"os"
 	"path/filepath"
 
 	"github.com/gin-gonic/gin"
@@ -12,7 +14,12 @@ func AvatarHandler(usersCollection *mongo.Collection, cwd string) gin.HandlerFun
 		avatarID := c.Param("filename")
 
 		filePath := filepath.Join(cwd, "PictureStorage", "Avatar", avatarID+".png")
-		c.FileAttachment(filePath, avatarID+".png")
 
+		if _, err := os.Stat(filePath); os.IsNotExist(err) {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Avatar not found"})
+			return
+		}
+
+		c.FileAttachment(filePath, avatarID+".png")
 	}
 }
